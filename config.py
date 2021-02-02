0c24ae05d44a4f6b74310c96059e0834f978ed06
Enrico <enrico.curiotto@bonitasoft.com>
2021-02-02 15:01:30 +0100

    added kube yaml

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 import connexion
-from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,5 +8,4 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 
 # Get the underlying Flask app instance
 app = connex_app.app
-# Initialize Marshmallow
-ma = Marshmallow(app)
+
--- readme.md
+++ readme.md
@@ -6,3 +6,12 @@ Constituted by a
 * Python deep neural network model 
 
 To execute the app: python app.py
+
+For Docker:
+
+sudo docker build -t yesno:latest .
+sudo docker run -d -p 5000:5000 yesno
+
+Kube:
+kubectl create -f kb8/yesno-deploy.yaml
+kubectl create -f kb8/yesno-service.yaml
